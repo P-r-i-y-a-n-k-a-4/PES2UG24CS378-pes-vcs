8558af3087bc4701f353dbbc716c1847bc7a7497
P-r-i-y-a-n-k-a-4 <priyanka.r.07490@gmail.com>
2026-04-21 13:01:14 +0530

    Add tree serialization and object writing logic
    Implement tree serialization and writing to object store.

--- tree.c
+++ tree.c
@@ -155,6 +155,18 @@ for (int i = 0; i < idx.count; i++) {
 }
 
 // temporary return (still incomplete)
-(void)id_out;
-return -1;
+void *data;
+size_t len;
+
+if (tree_serialize(&tree, &data, &len) != 0) {
+    return -1;
+}
+
+if (object_write(OBJ_TREE, data, len, id_out) != 0) {
+    free(data);
+    return -1;
+}
+
+free(data);
+return 0;
 }
